80e39d81f15c3e0d6bbd6d447aa90598d86e0bde
heidekeyi <1213173752@qq.com>
2021-08-28 04:33:59 -0400

    append highlight

--- init.el
+++ init.el
@@ -1,3 +1,22 @@
+;; M-x customize-group Return 输入某一个类别可以设置对应的种类
+;; 如：rigion 设置激活区域，hl-line 设置当前行高亮
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ ;; '(hl-line ((t (:background "#586F3E" :foreground "#dddddd"))))
+ ;; '(region ((t (:background "#863434" :foreground "#cccccc"))))
+ '(hl-line ((t (:background "#2e2e2e"))))
+ '(region ((t (:background "#2C55A7"))))
+  )
+
 ;; 文件默认保存为 utf-8
 (set-buffer-file-coding-system 'utf-8)
 (set-default buffer-file-coding-system 'utf8)
@@ -157,3 +176,4 @@
 
 ;; 补全引号、括号之类的
 (smartparens-global-mode t)
+
